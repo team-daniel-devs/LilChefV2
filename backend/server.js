f6f1d02b99128ba72e85ccaeadd755b6dfd0f1cd
dike12 <87872075+dike12@users.noreply.github.com>
2025-02-15 01:44:23 -0500

    from develop

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,21 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+<<<<<<< Updated upstream
 app.use(cors());
+=======
+app.use(cors(
+  {
+    origin:[
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://cookaing-da7d0.uc.r.appspot.com",
+    ],
+    method: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }
+));
+>>>>>>> Stashed changes
 app.use(express.json());
 
 // Root route
